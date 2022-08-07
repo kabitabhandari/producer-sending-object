0dfbc72d9277be7dae974966585571f10f579902
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2022-08-07 10:49:48 -0500

    update

---
+++ src/main/java/com/example/producersendingobject/ProducerSendingObjectApplication.java
@@ -0,0 +1,11 @@
+package com.example.producersendingobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerSendingObjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerSendingObjectApplication.class, args);
+    }
+}
